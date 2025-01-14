9b7f23eb2c85944158437f7dfe80b41dc3522fe7
zy <848175192@qqcom>
2025-01-14 15:49:14 +0800

    修复

---
+++ assets/css.html-DOCbu0l3.js
@@ -0,0 +1,23 @@
+import{_ as s,c as a,a as e,o as p}from"./app-CeApw9F0.js";const t={};function l(i,n){return p(),a("div",null,n[0]||(n[0]=[e(`<h3 id="流光字" tabindex="-1"><a class="header-anchor" href="#流光字"><span>流光字</span></a></h3><div class="language-css line-numbers-mode" data-highlighter="prismjs" data-ext="css" data-title="css"><pre><code><span class="line">    <span class="token selector">.sq_start</span> <span class="token punctuation">{</span></span>
+<span class="line">    <span class="token property">width</span><span class="token punctuation">:</span> 100%<span class="token punctuation">;</span></span>
+<span class="line">    <span class="token property">height</span><span class="token punctuation">:</span> 100%<span class="token punctuation">;</span></span>
+<span class="line">    <span class="token property">background-image</span><span class="token punctuation">:</span> <span class="token function">-webkit-linear-gradient</span><span class="token punctuation">(</span></span>
+<span class="line">        -40deg<span class="token punctuation">,</span></span>
+<span class="line">        transparent<span class="token punctuation">,</span></span>
+<span class="line">        transparent 42%<span class="token punctuation">,</span></span>
+<span class="line">        <span class="token function">rgba</span><span class="token punctuation">(</span>255<span class="token punctuation">,</span> 255<span class="token punctuation">,</span> 255<span class="token punctuation">,</span>0.5<span class="token punctuation">)</span> 50%<span class="token punctuation">,</span></span>
+<span class="line">        transparent 58%<span class="token punctuation">,</span></span>
+<span class="line">        transparent</span>
+<span class="line">    <span class="token punctuation">)</span><span class="token punctuation">;</span></span>
+<span class="line">    <span class="token property">-webkit-background-size</span><span class="token punctuation">:</span> 200% 100%<span class="token punctuation">;</span></span>
+<span class="line">    <span class="token property">-webkit-animation</span><span class="token punctuation">:</span> masked 1s infinite linear<span class="token punctuation">;</span></span>
+<span class="line">    <span class="token punctuation">}</span></span>
+<span class="line">    <span class="token atrule"><span class="token rule">@keyframes</span> masked</span> <span class="token punctuation">{</span></span>
+<span class="line">      <span class="token selector">0%</span> <span class="token punctuation">{</span></span>
+<span class="line">          <span class="token property">background-position</span><span class="token punctuation">:</span> 100% 0<span class="token punctuation">;</span></span>
+<span class="line">      <span class="token punctuation">}</span></span>
+<span class="line">      <span class="token selector">100%</span> <span class="token punctuation">{</span></span>
+<span class="line">          <span class="token property">background-position</span><span class="token punctuation">:</span> 0 0<span class="token punctuation">;</span></span>
+<span class="line">      <span class="token punctuation">}</span></span>
+<span class="line">    <span class="token punctuation">}</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2)]))}const o=s(t,[["render",l],["__file","css.html.vue"]]),u=JSON.parse('{"path":"/blogs/2021/css/css.html","title":"常用css","lang":"en-US","frontmatter":{"title":"常用css","date":"2021-12-29T00:00:00.000Z","tags":["css"],"category":["css"],"categories":[]},"headers":[{"level":3,"title":"流光字","slug":"流光字","link":"#流光字","children":[]}],"git":{},"filePathRelative":"blogs/2021/css/css.md"}');export{o as comp,u as data};
